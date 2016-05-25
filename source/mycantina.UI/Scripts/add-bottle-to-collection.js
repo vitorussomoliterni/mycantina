b2b4fc3f307038efac6083d317e0dba3445458d6
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-05-25 10:31:55 +1000

    Added js file and handlebars template for details bottle view.

---
+++ source/mycantina.UI/Scripts/add-bottle-to-collection.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    $(document).ready(function () {
+        var source = $("#user-select-template").html();
+        var template = Handlebars.compile(source);
+
+        $("#add-collection-btn").click(function (e) {
+            $("#select-user-form").append(template);
+        });
+    });
+})();
