2227adbea52f5adcf8792b4902e555762ebdb8f1
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-05-30 11:11:26 +1000

    Javascript added for review.

---
+++ source/mycantina.UI/Scripts/reviewComment.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    $(document).ready(function () {
+        var source = $("#review-comment-template").html();
+        var template = Handlebars.compile(source);
+
+        $("#review-button").click(function (e) {
+            $("#review-button").hide()
+            $("#review-comment").html(template);
+        });
+    });
+})();
